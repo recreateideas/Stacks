4b5339d20d352bcdf933aab0d11082c6a1ea774e
Claudio <claudio.deangelis@cloud-iq.com>
2020-04-12 20:53:53 +1000

    removed test lines

--- main-process/docker-events/setup-docker-events-listener.js
+++ main-process/docker-events/setup-docker-events-listener.js
@@ -1,16 +1,10 @@
 const childProcess = require('child_process');
 const { EventEmitter } = require('events');
 const eventHandler = require('./event-handler');
-const { getDockerCompose } = require('../utils');
 
 const eventEmitter = new EventEmitter();
 
 const { stdout: dockerEvents } = childProcess.spawn('docker', ['events', '--format', '{{json .}}']);
-
-const dockerComposeYaml = getDockerCompose(['/Users/claudio/claud.io/docker/docker-compose.dev.yml'], 'utf8');
-
-console.log(dockerComposeYaml);
-// console.log(inspectContainer('db')[0].Config.Labels['com.docker.compose.project.config_files']);
 
 let inDebounce;
 let eventsList = [];
